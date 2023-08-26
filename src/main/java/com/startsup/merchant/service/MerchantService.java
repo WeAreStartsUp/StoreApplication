ed40e4f9a2571bb139dbfee8d9d3c689e172127f
vinothgopi <vinothgopi@hotmail.com>
2023-08-26 14:12:21 +0530

    feat(code): merchant service, mysql database

---
+++ src/main/java/com/startsup/merchant/service/MerchantService.java
@@ -0,0 +1,50 @@
+package com.startsup.merchant.service;
+
+import com.startsup.merchant.entities.Merchants;
+import com.startsup.merchant.database.repository.MerchantsRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class MerchantService {
+
+    private final MerchantsRepository merchantsRepository;
+
+    public MerchantService(MerchantsRepository merchantsRepository) {
+        this.merchantsRepository = merchantsRepository;
+    }
+
+    public List<Merchants> getAllMerchants() {
+        List<Merchants> merchants;
+        try {
+            merchants = merchantsRepository.findAll();
+        } catch (Exception e) {
+            merchants = new ArrayList<>();
+        }
+        return merchants;
+    }
+
+    public Optional<Merchants> getMerchantFromId(int id) {
+        Optional<Merchants> merchant;
+        try {
+            merchant = merchantsRepository.findById(id);
+        } catch (Exception e) {
+            merchant = Optional.empty();
+        }
+        return merchant;
+    }
+
+    public Optional<Merchants> updateMerchant(Merchants merchant) {
+        try {
+            merchant = merchantsRepository.save(merchant);
+            return Optional.of(merchant);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+}

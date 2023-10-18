87ff2b19061bb1bd61bb014baa5a3afcc0f93260
vinothgopi <vinothgopi@hotmail.com>
2023-10-18 16:47:19 +0530

    feat(code): update

--- src/main/java/com/startsup/merchant/controller/ItemController.java
+++ src/main/java/com/startsup/merchant/controller/ItemController.java
@@ -8,7 +8,10 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,6 +19,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,6 +47,19 @@ public class ItemController {
         List<Items> items = itemService.getAllItems();
         logger.info("Response={}", items);
         return ResponseEntity.ok(items);
+    }
+
+    @GetMapping("/items/template")
+    public ResponseEntity<Object> fetchItemsTemplate() throws IOException {
+        logger.info("Request Received");
+        File file = ResourceUtils.getFile("classpath:" + "items.xlsx");
+        ByteArrayResource byteArrayResource = new ByteArrayResource(Files.readAllBytes(Path.of(file.getPath())));
+
+        return ResponseEntity.ok()
+                .contentLength(file.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header("Content-Disposition", "attachment; filename=itemtemplate.xlsx")
+                .body(byteArrayResource);
     }
 
     @GetMapping("/items/{id}")
